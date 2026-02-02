a17ca2f1dd1ff2b4ac47e245d791f229129c1a41
Dubby <78522474+dubstepdragon@users.noreply.github.com>
2026-02-02 17:05:12 -0500

    Update AnimateEulerAnglesCoroutine.cs (#4)
    * Update AnimateEulerAnglesCoroutine.cs
    
    - Removed base Setup since it was a little redundant
    - changed the StartCoroutine method to set the start and end that's given to it rather than have it in the Setup override since this class no longer overrides it.
    - Added an additional Start coroutine method to run with the current values in the list. without setting the Start and end. 
    - Added a Start and End getter and setter for external modification of the private variables (in case future use elsewhere dictates you need to update variables prior to running the co-routine)
    
    * Update AnimateEulerAnglesCoroutine.cs
    
    - Removed base Setup since it was a little redundant
    - changed the StartCoroutine method to set the start and end that's given to it
    - Added an additional Start coroutine method to run with the current values in the list. without setting the Start and end. 
    - fixed some naming conventions to be more appropriate for internal parameters vs class parameters.

--- Packages/com.caeden117.udoncoroutines/Runtime/Coroutines/Animations/Transforms/AnimateEulerAnglesCoroutine.cs
+++ Packages/com.caeden117.udoncoroutines/Runtime/Coroutines/Animations/Transforms/AnimateEulerAnglesCoroutine.cs
@@ -1,55 +1,95 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// An Udon Coroutine that animates the world space euler rotation of a target <see cref="Transform"/>.
+///
+/// Additional edits by DubbyDragon
 /// </summary>
 /// <remarks>
 /// This Coroutine internally converts Euler rotations into <see cref="Quaternion"/>s, then animates those.
 /// </remarks>
 public class AnimateEulerAnglesCoroutine : AnimatedUdonCoroutine
 {
+    
+#region VARIABLES
     [Header("Euler Angles Coroutine")]
     [Tooltip("Transform which will be animated.")]
     [SerializeField]
     private Transform target;
-    
     [Tooltip("Starting rotation for the animation.")]
     [SerializeField]
     private Vector3 start;
-    
     [Tooltip("Ending rotation for the animation.")]
     [SerializeField]
     private Vector3 end;
-
-    private Quaternion startRotation;
-    private Quaternion endRotation;
+    private Quaternion _startRotation;
+    private Quaternion _endRotation;
+#endregion
     
+#region GETTERS_SETTERS
+        
+    public Vector3 GetEnd()
+    {
+        return end;
+    }
+    
+    public void SetEnd(Vector3 endEulerAngles)
+    {
+        end = endEulerAngles;
+    }
+    
+    public Vector3 GetStart()
+    {
+        return end;
+    }
+    
+    public void SetStart(Vector3 startEulerAngles)
+    {
+        end = startEulerAngles;
+    }
+#endregion
+    
+#region COROUTINE_METHODS
     /// <summary>
     /// Starts this Udon Coroutine with overridden start and ending points, duration, and callback.
     /// </summary>
-    /// <param name="start">Override starting vector.</param>
-    /// <param name="end">Override ending vector.</param>
+    /// <param name="startEulerAngles">Override starting vector.</param>
+    /// <param name="endEulerAngles">Override ending vector.</param>
     /// <param name="duration">Overridden animation length, in seconds</param>
-    /// <param name="callback">Component to recieve events during this Coroutine (can be null).</param>
-    public void StartUdonCoroutine(Vector3 start, Vector3 end, float duration = 1f,
-        BaseUdonCoroutineCallback callback = null)
+    /// <param name="endCallback">Component to recieve events during this Coroutine (can be null).</param>
+    public void StartUdonCoroutine(Vector3 startEulerAngles, Vector3 endEulerAngles, float duration = 1f,
+        BaseUdonCoroutineCallback endCallback = null)
     {
-        this.start = start;
-        this.end = end;
+        this.start = startEulerAngles;
+        this.end = endEulerAngles;
         
-        StartUdonCoroutine(duration, callback);
-    }
-
-    protected override void Setup()
-    {
-        base.Setup();
-        startRotation = Quaternion.Euler(start);
-        endRotation = Quaternion.Euler(end);
+        _startRotation = Quaternion.Euler(start);
+        _endRotation = Quaternion.Euler(end);
+        
+        StartUdonCoroutine(duration, endCallback);
     }
     
+
+    /// <summary>
+    /// A mirror to the base callback allowing you to not have to add a start and end parameter. that calls the base function
+    /// This allows for more flexible usage
+    /// </summary>
+    /// <author>DubstepDragon</author>
+    /// <param name="duration"></param>
+    /// <param name="endCallback"></param>
+    public void StartUdonCoroutineRaw(float duration = 1f,
+        BaseUdonCoroutineCallback endCallback = null)
+    {
+        StartUdonCoroutine(start, end, duration, endCallback);
+    }
+#endregion
+
+#region OVERRIDES
     protected override void UpdateAnimation(float progress)
     {
-        target.rotation = Quaternion.Lerp(startRotation, endRotation, progress);
+        target.rotation = Quaternion.Lerp(_startRotation, _endRotation, progress);
     }
+#endregion
 }
